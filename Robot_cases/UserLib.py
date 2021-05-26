3c97c0e45305f683869e6a31f632472fb45f5da0
andreyyuryev <yuryev@yandex.ru>
2021-05-26 16:03:48 +0300

    sap test case

--- Robot_cases/UserLib.py
+++
@@ -1,9 +0,0 @@
-class UserLib():
-    def __init__(self):
-        self.first_title = 'XPL(1)/200 SAP Easy Access'
-
-    def is_first(self, title):
-        if title == self.first_title:
-            print(f'The title of window is {title}')
-        else:
-            raise AssertionError(f'The title of window shoul be {self.first_title} but is {title}')
