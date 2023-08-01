176e66117c119027e96cb43fa6a62994173941ec
Andrey <yuryev@yandex.ru>
2023-08-01 15:51:56 +0300

    starter

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Andrey Yuryev',
+    author_email='',
+    description='Python project'
+)
